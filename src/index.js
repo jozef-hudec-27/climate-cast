49ce365f0049e9ee0e0fa932b3bced0e9635dcb0
Jozef Hudec <jojohudec27@gmail.com>
2023-02-12 11:08:42 +0100

    Create OpenWeatherMap for working with API

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './assets/styles/style.scss'
+import OpenWeatherMap from './open_weather_map'
+
+OpenWeatherMap.geoCodingAPI('London').then(async cities => {
+  let { lat, lon } = cities[0]
+
+  const weatherData = await OpenWeatherMap.weatherForecastAPI(lat, lon)
+  console.log(weatherData)
+})
---
+++ src/open_weather_map.js
@@ -0,0 +1,27 @@
+export default class OpenWeatherMap {
+  static API_KEY = '48bceb5392f933e1b627b36faed19788'
+  static prefix = 'https://api.openweathermap.org'
+
+  static getUrlFor = (path) => {
+    return `${this.prefix}${path}&appid=${this.API_KEY}`
+  }
+
+  static geoCodingAPI = async (q) => {
+    // q={city name},{state code},{country code}
+    const url = this.getUrlFor(`/geo/1.0/direct?q=${q}&limit=5`)
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+  }
+
+  static weatherForecastAPI = async (lat, lon) => {
+    const url = this.getUrlFor(`/data/2.5/forecast?lat=${lat}&lon=${lon}`)
+
+    const response = await fetch(url)
+    const data = await response.json()
+
+    return data
+  }
+}
