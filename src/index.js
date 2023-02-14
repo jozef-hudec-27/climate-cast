2577fb189e0c5286ce66b679e709561b451881b1
Jozef Hudec <jojohudec27@gmail.com>
2023-02-14 19:35:25 +0100

    Style city search results

--- src/index.js
+++ src/index.js
@@ -8,9 +8,12 @@ Dom.byId('search-city').addEventListener('submit', (e) => {
   OpenWeatherMap.geoCodingAPI(Dom.byId('city-input').value).then(async (cities) => {
     Dom.byId('cities').innerHTML = ''
     cities.forEach((city) => {
-      let container = Dom.newElement('div', ['city'], '')
+      let container = Dom.newElement('button', ['city'], '')
+      let flag = Dom.newElement('img')
+      flag.src = `https://www.countryflagicons.com/FLAT/24/${city.country.toUpperCase()}.png`
+      flag.alt = `${city.country.toUpperCase()} flag`
       let heading = Dom.newElement('h2', [], '', city.name)
-      container.appendChild(heading)
+      Dom.addChildrenTo(container, [flag, heading])
       Dom.byId('cities').appendChild(container)
     })
   })
